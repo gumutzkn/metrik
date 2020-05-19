0f8bb721bcac810821dc45b1838a5962e34f2710
Görkem <gumutzkn@gmail.com>
2020-05-19 17:51:09 +0300

    changed

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-let ul = document.querySelector(".sayılar");
-let li = document.querySelectorAll(".sayılar li");
-let div = document.querySelector(".bölüm-2");
+// let ul = document.querySelector(".sayılar");
+// let li = document.querySelectorAll(".sayılar li");
+// let div = document.querySelector(".bölüm-2");
 
-li.forEach(el => {
-  el.addEventListener("click", function() {
-    ul.querySelector(".active").classList.remove("active");
+// var i = 0;
 
-    el.classList.add("active");
+// li.forEach(el => {
+//   el.addEventListener("click", function() {
+//     i++;
 
-    div.classList.add("içeriye");
-  });
-});
+//     ul.querySelector(".active").classList.remove("active");
+
+//     el.classList.add("active");
+
+//     addDiv();
+//   });
+// });
+
+// const addDiv = () => {
+//   if (i === 1) {
+//     div.classList.add(".içeriye");
+//   }
+// };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("bölüm");
+  var sayı = document.getElementsByClassName("sayı");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < sayı.length; i++) {
+    sayı[i].className = sayı[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "inherit";
+  sayı[slideIndex - 1].className += " active";
+}
